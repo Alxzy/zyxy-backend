0dc52f398ee4c7d2b4be3ffdf3aff39b59edc914
furina <furina@zz.com>
2024-03-09 16:36:11 +0800

    initial state

---
+++ src/test/java/com/zy/zyxy/service/UserServiceTest.java
@@ -0,0 +1,133 @@
+package com.zy.zyxy.service;
+
+
+import com.zy.zyxy.model.dto.User;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 用户服务测试
+ *
+ * @author zy
+ * 
+ */
+@SpringBootTest
+@Slf4j
+public class UserServiceTest {
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 测试添加用户
+     */
+    @Test
+    public void testAddUser() {
+        User user = new User();
+        user.setUsername("dogYupi");
+        user.setUserAccount("123");
+        user.setAvatarUrl("https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png");
+        user.setGender(0);
+        user.setUserPassword("xxx");
+        user.setPhone("123");
+        user.setEmail("456");
+        boolean result = userService.save(user);
+        System.out.println(user.getId());
+        Assertions.assertTrue(result);
+    }
+
+    // https://www.code-nav.cn/
+
+    /**
+     * 测试更新用户
+     */
+    @Test
+    public void testUpdateUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("dogYupi");
+        user.setUserAccount("123");
+        user.setAvatarUrl("https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png");
+        user.setGender(0);
+        user.setUserPassword("xxx");
+        user.setPhone("123");
+        user.setEmail("456");
+        boolean result = userService.updateById(user);
+        Assertions.assertTrue(result);
+    }
+
+    /**
+     * 测试删除用户
+     */
+    @Test
+    public void testDeleteUser() {
+        boolean result = userService.removeById(1L);
+        Assertions.assertTrue(result);
+    }
+
+
+    /**
+     * 测试获取用户
+     */
+    @Test
+    public void testGetUser() {
+        User user = userService.getById(1L);
+        Assertions.assertNotNull(user);
+    }
+
+    /**
+     * 测试用户注册
+     */
+    @Test
+    void userRegister() {
+        String userAccount = "cxkkkkkkk";
+        String userPassword = "12345678";
+        String checkPassword = "12345678";
+        String planetCode = "10";
+        long result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(11, result);
+//        userAccount = "yu";
+//        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+//        Assertions.assertEquals(-1, result);
+//        userAccount = "zy";
+//        userPassword = "123456";
+//        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+//        Assertions.assertEquals(-1, result);
+//        userAccount = "yu pi";
+//        userPassword = "12345678";
+//        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+//        Assertions.assertEquals(-1, result);
+//        checkPassword = "123456789";
+//        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+//        Assertions.assertEquals(-1, result);
+//        userAccount = "dogYupi";
+//        checkPassword = "12345678";
+//        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+//        Assertions.assertEquals(-1, result);
+//        userAccount = "zy";
+//        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+//        Assertions.assertEquals(-1, result);
+    }
+
+    @Test
+    void testUserRegister() {
+    }
+
+    @Test
+    void searchUsersByTags() {
+        List<String> list = Arrays.asList("java", "python");
+        List<User> users = userService.searchUsersByTags(list);
+        for (User user : users) {
+            System.out.println("查询到的用户有：" + user.toString());
+        }
+        Assert.assertNotNull(users);
+    }
+}
