7b562d4a7f5f2ec784a75bb44fc89f0cbcf0db98
furina <furina@zz.com>
2024-04-12 20:51:37 +0800

    Production version 1.0.1

---
+++ src/main/java/com/zy/zyxy/mapper/TagMapper.java
@@ -0,0 +1,18 @@
+package com.zy.zyxy.mapper;
+
+import com.zy.zyxy.model.dto.Tag;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Administrator
+* @description 针对表【tag(标签)】的数据库操作Mapper
+* @createDate 2024-04-02 20:45:37
+* @Entity com.zy.zyxy.model.dto.Tag
+*/
+public interface TagMapper extends BaseMapper<Tag> {
+
+}
+
+
+
+
